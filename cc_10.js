c538d3bd6b32d0ebfe8caac2ffa9f3d4927469b3
ZachRober <nolethedgehogdie@gmail.com>
2024-10-17 13:12:29 -0400

    size selector

--- cc_10.js
+++ cc_10.js
@@ -1,3 +1,12 @@
+const productName = document.getElementById('product-name');
+const productPrice = document.getElementById('product-price');
 const totalPrice = document.getElementById('total-price');
 const sizeSelector = document.getElementById('size-selector');
 const purchase = document.getElementById('purchase');
+
+sizeSelector.addEventListener('change',(event)=>{
+const realPirce = event.target.value;
+productPrice.textContent = `$${realPirce}`;
+});
+
+
